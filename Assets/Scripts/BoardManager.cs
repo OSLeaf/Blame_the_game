1809605f084c9d494884718d967f3d842c878c47
Disdisz <97905204+Disdisz@users.noreply.github.com>
2023-07-09 17:22:07 +0300

    playerupdatekorjaus

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -128,7 +128,7 @@ public class BoardManager : MonoBehaviour
         }
         catch (System.Exception e) 
         {
-            
+            Debug.Log(e);
         }   
 
 
@@ -150,7 +150,7 @@ public class BoardManager : MonoBehaviour
     private void UpdatePlayerStats()
     {
         int i;
-        int[] money = (int[])playerValues.Select(p => p.money);
+        List<int> money = playerValues.Select(p => p.money).ToList<int>();
         int suurin = money.Max();
         int suurinInd = money.ToList().IndexOf(suurin);
         List<int> vihattavat = new List<int>();
@@ -173,19 +173,23 @@ public class BoardManager : MonoBehaviour
             {
                 foreach (int saa in saalittavat)
                 {
-                    playerValues[saa].relationships["" + ((i - saa + 4) % 4)] += 5;
+                    string id = "" + ((i - saa + 4) % 4);
+                    if (id != "0") { playerValues[saa].relationships[id] += 5; };
                 }
                 playerValues[i].happiness -= 2;
                 playerValues[i].vitutus += 5;
                 foreach (int vih in vihattavat)
                 {
                     string id = "" + ((vih - i + 234632) % 4);
-                    playerValues[i].relationships[id] -= 5;
+                    if (id != "0") { playerValues[i].relationships[id] -= 5; };
                 }
                 saalittavat.Add(i);
                 foreach (int saa in saalittavat)
                 {
-                    playerValues[i].relationships["" + ((saa - i + 4) % 4)] += 5;
+                    string id = "" + ((saa - i + 4) % 4);
+                    if (id != "0")
+                    { playerValues[i].relationships[id] += 5; }
+
                 }
             } 
             else
@@ -193,13 +197,13 @@ public class BoardManager : MonoBehaviour
                 foreach (int vih in vihattavat)
                 {
                     string id = "" + ((vih - i + 497524) % 4);
-                    playerValues[i].relationships[id] -= 6;
+                    if (id != "0") { playerValues[i].relationships[id] -= 6; }
                 }
                 int j;
                 for ( j = 0; j<4; j++)
                 {
                     string id = "" + ((j - i + 497524) % 4);
-                    playerValues[i].relationships[id] += 2;
+                    if (id != "0") { playerValues[i].relationships[id] += 2; }
                 }
             }
         }
